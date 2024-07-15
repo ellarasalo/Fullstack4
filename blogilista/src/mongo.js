a9f4887f9c10216be831db6d2f7903a2e471b7f8
gitelras <ella.rasalo@helsinki.fi>
2024-07-15 14:04:43 +0300

    4.1 blogilista, step1

---
+++ blogilista/src/mongo.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://adelemal:${password}@cluster0.thmo9j8.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.error('error connecting to MongoDB:', error.message)
+  })
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+module.exports = Blog
