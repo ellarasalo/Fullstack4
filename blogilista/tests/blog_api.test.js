663f72633be13fdc6b043ee974f3ee15c4f30caf
gitelras <ella.rasalo@helsinki.fi>
2024-07-15 18:25:05 +0300

    4.8: blogilistan testit, step 1

---
+++ blogilista/tests/blog_api.test.js
@@ -0,0 +1,44 @@
+const { test, describe, beforeEach, after } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'First Blog',
+    author: 'John Doe',
+    url: 'http://example.com/first',
+    likes: 5
+  },
+  {
+    title: 'Second Blog',
+    author: 'Jane Doe',
+    url: 'http://example.com/second',
+    likes: 10
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogs)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+  assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+after(() => {
+  mongoose.connection.close()
+})
--- blogilista/tests/list_helper.test.js
+++ blogilista/tests/list_helper.test.js
@@ -1,4 +1,4 @@
-const { test, describe } = require('node:test')
+const { test, describe, beforeEach, afterAll } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
