80f451217633a3e4c665226da4665c22a6a10974
gitelras <ella.rasalo@helsinki.fi>
2024-07-16 15:44:10 +0300

    4.15: blogilistan laajennus, step3

--- blogilista/app.js
+++ blogilista/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 const app = express()
 
@@ -19,14 +20,16 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
-app.use('/api/blogs', blogsRouter)
 
-app.use((request, response, next) => {
+app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+
+app.use((request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 })
 
 app.use((error, request, response, next) => {
-  console.error(error.message)
+  logger.error(error.message)
 
   if (error.name === 'CastError' && error.kind === 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
